e595297580ff41c3a73ac47ca44c4560600566b1
learn-dumboo24 <sanketjha160@gmail.com>
2025-04-03 09:51:49 +0530

    trail commit

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -3,12 +3,7 @@ import React from "react";
 const HeroSection = () => {
     return (
         <section className = "relative flex flex-col items-center justify-center h-screen bg-gradient-to-r from-gray-900 via-black to-fuchsia-950 text-white overflow-hidden">
-            <div className="absolute inset-0 z-0">
-                <svg className="w-full h-full" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
-                
-                </svg>
-            </div>
-
+            
             <div className = "relative z-10 text-center">
                 <h1 className = "text-6xl font-extrabold tracking-wide drop-shadow-lg animate-fade-in">
                     SkillConnect : Elevate Your Learning
@@ -19,11 +14,13 @@ const HeroSection = () => {
             </div>
 
             <div className = "mt-6 flex gap-4">
-                <button className = "px-6 py-3 bg-gradient-to-r from-pink-500 to-purple-500 rounded-lg text-white text-lg font-semibold shadow-lg transition-transform hover:scale-105">
+                <button className = "relative px-6 py-3 bg-gradient-to-r from-pink-500 to-purple-500 rounded-lg text-white text-lg font-semibold shadow-lg transform transition-all duration-300 hover:scale-105">
                     Get Started
+                    <div className= "absolute inset-0 bg-purple-700 opacity-20 blur-2xl -z-10"></div>
                 </button>
-                <button className = "px-6 py-3 border-gray-500 rounded-lg text-lg font-semibold text-gray-300 transition-transform hover:scale-105">
+                <button className = "relative px-6 py-3 border border-gray-500 rounded-lg text-lg font-semibold text-gray-300 transition-all duration-300 transform hover:scale-105">
                     Learn More
+                    <div className= "absolute inset-0 bg-purple-700 opacity-20 blur-2xl -z-10"></div>
                 </button>
             </div>
         </section>
@@ -31,3 +28,4 @@ const HeroSection = () => {
 };
 
 export default HeroSection;
+        
